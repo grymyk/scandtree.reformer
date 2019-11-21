83691b2e84da4eba9145e1e0a46a808ba0e5813e
grymyk <grymyk@gmail.com>
2019-11-21 17:05:40 +0200

    first commit

---
+++ src/Former/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './css/main.scss';
+import './css/tree.scss';
+
+function App() {
+  return (
+    <div className="App">
+      hello Scandtree
+    </div>
+  );
+}
+
+export default App;
